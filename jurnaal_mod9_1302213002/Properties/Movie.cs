146e8b6549113b9049285432b974af071dae034f
bintangrayhan <bintangrayhan7@gmail.com>
2023-04-17 06:48:24 +0700

    Add project files.

---
+++ jurnaal_mod9_1302213002/Properties/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace jurnaal_mod9_1302213002.Properties
+{
+    public class Movie
+    {
+        public String title { get; private set; };
+        public String director { get; private set; }
+        public String stars { get; private set; } 
+        public String description { get; private set; }   
+    }
+}
